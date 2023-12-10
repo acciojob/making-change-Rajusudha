d3d0ef664c0e8a92df86e14a9c3310a2b8bcdb21
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-10 15:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+  if (amount <= 0 || amount >= 100) {
+    return "Invalid input. Amount must be between 1 and 99 cents.";
+  }
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+  const quarters = Math.floor(amount / 25);
+  const remainingAfterQuarters = amount % 25;
+  
+  const dimes = Math.floor(remainingAfterQuarters / 10);
+  const remainingAfterDimes = remainingAfterQuarters % 10;
+  
+  const nickels = Math.floor(remainingAfterDimes / 5);
+  const pennies = remainingAfterDimes % 5;
+
+  const change = {
+    "q": quarters,
+    "d": dimes,
+    "n": nickels,
+    "p": pennies,
+  };
+
+  return change;
+}
+
+// Examples
+console.log(makeChange(47)); // Output: { "q": 1, "d": 2, "n": 0, "p": 2 }
+console.log(makeChange(24)); // Output: { "q": 0, "d": 2, "n": 0, "p": 4 }
+
